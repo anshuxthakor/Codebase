4dd51100b6b7ebae5943dc01a60eff8acf6d31bc
Thakor DakshKumar <anshuxthakor@gmail.com>
2026-06-13 13:37:57 +0530

    Destructuring Explained

--- JavaScript/07/script.js
+++ JavaScript/07/script.js
@@ -44,7 +44,7 @@ console.log(user.spouse.skills[1]);
 
 
 // Storing a function inside an object is called a method. 
-var obj = {
+var robo = {
   name: 'Chiti',
   ram: '20mb',
   age: 1,
@@ -59,17 +59,17 @@ var obj = {
     console.log("Hi, I am " + this.name + ' Robo!');
   }
 }
-obj.sayHi(); 
+robo.sayHi(); 
 
 // Passing parameters to a method
-var obj1 = {
+var actor = {
   user: 'Anushka',
   greet: function(person) {
     console.log(person + ", " + this.user + " says Hello!");
   }
 }
 
-obj1.greet('Virat'); // Virat, Anushka says Hello!
+actor.greet('Virat'); // Virat, Anushka says Hello!
 
 
 
@@ -124,4 +124,122 @@ console.log(arey);
 
 // Object.freeze(arey);
 arey[0] = 20; // This will not work because we have frozen the array.
+
+// Destructuring In JavaScript
+
+// Destructuring is a convenient way of extracting multiple values from data stored in objects and arrays.
+
+// With Arrays
+var array = [11, 22, 33, 44];
+
+// Basic Destructuring --> Assigning values to variables based on the position
+var [a, b] = array; // a = 11, b = 22 remaining values will be ignored
+console.log(a, b);
+
+// Skipping Values --> Using Empty Space and Comma
+var [x, , y] = array; // x = 11, y = 33, the value at index 1 will be ignored 
+console.log(x, y);
+
+// Rest Operator --> ...rest (Remaining Elements)
+// Rest operator can only be used at the end of the destructuring assignment
+
+var [p, q, ...rest] = array; // p = 11, q = 22, rest = [33, 44] 
+console.log(p, q);
+console.log(rest);
+
+// ... on left side ---> rest operator
+// ... on right side ---> spread operator
+
+
+// With Objects
+var obj = {
+  name: "Anshu",
+  age: 20,
+  batch: "Cohort 3.0"
+}
+
+// Basic Destructuring --> Assigning values to variables based on the key name
+var {name, age} = obj; // name = "Anshu", age = 20
+console.log(name, age);
+
+// In arrays we can use any variable name for destructuring but in objects we have to use the same variable name as the key name in the object.
+
+// To avoid this we can use aliasing --> variableName: newVariableName
+var {name: username, age: userage} = obj;
+console.log(username, userage);
+
+// Rest Operator --> ...rest (Remaining Properties)
+var { name, ...restObj } = obj;
+console.log(restObj); // restObj will contain the remaining properties
+
+
+// Spread Operator --> ...spread (Copying an Object)
+var newArray = [...array, 55]; // This will create a SHALLOW COPY of the array and add a new value 55 at the end of the array
+
+newArray[0] = 100; // This will not change the original array 
+console.log(array); // Original array remains unchanged
+console.log(newArray); // New array with the updated value
+
+
+var newObj = { ...obj, city: "Delhi" }; // This will create a SHALLOW COPY of the object and add a new property city
+newObj.name = "Anushka"; // This will not change the original object
+console.log(obj); // Original object remains unchanged
+console.log(newObj); // New object with the updated value and new property
+
+// Now what is a SHALLOW COPY ?
+
+// A Shallow copy only duplicates the top-level properties, while a deep copy recursively duplicates every level, creating a completely independent objects.
+
+// A shallow copy is a copy of an object or array that shares the same reference to the nested objects or arrays.
+// Therefore, if we change the nested objects or arrays in the new object or array, it will also change in the original object or array because they are referencing the same nested objects or arrays.
+
+// Therefore we create a deep copy to avoid this issue.
+
+var originalObj = {
+  name: "Harsh",
+  age: 20,
+  marks: 65,
+  college: {
+    name: "LNCT",
+    student: 20000
+  },
+  skills: ['JavaScript', 'React', 'Node.js']
+}
+
+// JSON.stringify() --> It converts a JavaScript object into a JSON string.
+// JSON.parse() --> It converts a JSON string back into its original form.
+
+var str = JSON.stringify(originalObj); // Convert the object to a JSON string
+var deepCopiedObj = JSON.parse(str); // Convert the JSON string back to an object
+
+deepCopiedObj.name = "Anshu Thakor";
+deepCopiedObj.marks = 97;
+deepCopiedObj.college.name = "IIT";
+deepCopiedObj.skills.push('Python');
+
+console.log(originalObj);
+console.log(deepCopiedObj);
+
+var originalArray = [1, {name: 'Anshu', age: 20}, 3, [4, 5]];
+
+var oarr = [...originalArray]; // SHALLOW COPY of the original array
+
+// Things are nested here therefore it will also change the original array
+oarr[0] = 10;
+oarr[1].name = "Harsh"; 
+oarr[3].push(6); 
+
+console.log(oarr)
+console.log(originalArray);
+
+// DEEP COPY of the original array using JSON methods
+var strArr = JSON.stringify(originalArray);
+var deepCopiedArray = JSON.parse(strArr);
+
+deepCopiedArray[0] = 100;
+deepCopiedArray[1].name = "Harsh";
+deepCopiedArray[3].push(7);
+
+console.log(deepCopiedArray);
+console.log(originalArray);
 
