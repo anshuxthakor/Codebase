575fdad6f6dc27040d662a41c7ed3f556638d6b4
Thakor DakshKumar <anshuxthakor@gmail.com>
2026-06-24 12:30:04 +0530

    Prototypes

--- JavaScript/09/script.js
+++ JavaScript/09/script.js
@@ -87,3 +87,59 @@ var student4 = {
 
 var getIntroBound = student1.getIntro.bind(student4);
 getIntroBound(); // Bob Brown
+
+// Prototypical Chaining Or Inheritance
+
+var college = {
+  name: 'IIT Bombay',
+  rating: 'A+',
+  students: 5000,
+  saySlogan: function () {
+    console.log('We are the best college in India');
+  }
+};
+
+var branch = {
+  name: 'CSE',
+  rating: 'A++',
+  students: 500,
+  subjects: ['TOC', 'SD', 'CD']
+};
+
+var user = {
+  name: 'Anshu Thakor',
+  age: 20,
+  marks: 98
+};
+
+user.__proto__ = branch;
+branch.__proto__ = college;
+
+console.log(user);
+
+// There is a method for checking the prototype of an object
+
+// Array --> Array --> Object --> null
+// Object --> Object --> null
+// Function --> Function --> Object --> null
+
+console.log(Array.prototype)
+console.log(Object.prototype)
+console.log(Function.prototype)
+
+// These works with only Array, Object and Function.
+
+// If we want to use .prototype we can create a constructor function.
+
+function Person(firstName, lastName, age) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+}
+console.log(Person.prototype); // Person {}
+
+var person1 = new Person('John', 'Doe', 25);
+var person2 = new Person('Jane', 'Smith', 30);
+
+console.log(person1);
+console.log(person2);
