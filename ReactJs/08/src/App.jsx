2cb23b778e54d3fc4746cd30441a3051aa78105c
Thakor DakshKumar <anshuxthakor@gmail.com>
2026-07-21 21:04:29 +0530

    State Uplifting - Code Added

--- ReactJs/08/src/App.jsx
+++ ReactJs/08/src/App.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
-import Login from './components/Login'
-import Register from './components/Register'
+import React, { useState } from "react";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 const App = () => {
   const [toggle, setToggle] = useState();
+  const [users, setUsers] = useState([]);
 
   return (
-    <div className='app'>
-      {toggle ? <Login setToggle={setToggle} /> : <Register setToggle={setToggle} />}
+    <div className="app">
+      {toggle ? (
+        <Login setToggle={setToggle} />
+      ) : (
+        <Register setToggle={setToggle} setUsers={setUsers} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- ReactJs/08/src/components/Register.jsx
+++ ReactJs/08/src/components/Register.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { use, useState } from "react";
 
-const Register = ({ setToggle }) => {
+const Register = ({ setToggle, setUsers }) => {
+  const [formData, setFormData] = useState({username: "", email: "", password: ""});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUsers((prev) => [...prev, formData]);
+    setFormData({ username: "", email: "", password: "" });
+    console.log("User registered:", formData);
+  }
+
   return (
     <div className="page">
       <div className="left">
@@ -15,14 +29,39 @@ const Register = ({ setToggle }) => {
       </div>
       <div className="right">
         <h1>Sign Up</h1>
-        <form action="">
-          <input type="text" placeholder="Username" autoComplete="username" />
-          <input type="email" placeholder="Email" autoComplete="email" />
-          <input type="password" placeholder="Password" autoComplete="new-password" />
+        <form onSubmit={handleSubmit}>
+          <input
+            required
+            value={formData.username}
+            name="username"
+            onChange={handleChange}
+            type="text"
+            placeholder="Username"
+            autoComplete="username"
+          />
+          <input
+            required
+            value={formData.email}
+            name="email"
+            onChange={handleChange}
+            type="email"
+            placeholder="Email"
+            autoComplete="email"
+          />
+          <input
+            required
+            value={formData.password}
+            name="password"
+            onChange={handleChange}
+            type="password"
+            placeholder="Password"
+            autoComplete="new-password"
+          />
           <button>Sign Up</button>
         </form>
         <p>
-          Already have an account?<span onClick={() => setToggle((prev) => !prev)}>Login</span>
+          Already have an account?
+          <span onClick={() => setToggle((prev) => !prev)}>Login</span>
         </p>
       </div>
     </div>
